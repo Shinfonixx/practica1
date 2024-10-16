0fadd043752cb0b9ebb53f0be7334a800e277dda
Shinfonixx <Dani05cartagena@gmail.com>
2024-10-16 19:04:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express' //framework de JS
 import mongoose from 'mongoose' //libreria para conectar con mongo
 
-const Animal = mongoose.model('Animal', new mongoose.Schema({
-  tipo: String,
-  estado: String,
+const Videojuego = mongoose.model('Videojuego', new mongoose.Schema({
+  titulo: String,
+  genero: String,
 }))
 
 const app = express()
@@ -11,14 +11,14 @@ const app = express()
 mongoose.connect('mongodb://isen:dwes@practica1:27017/miapp?authSource=admin')
 
 app.get('/', async (_req, res) => {
-  console.log('listando... animales...')
-  const animales = await Animal.find(); //busca a todos los animales en base de datos y los devuelve
-  return res.send(animales)
+  console.log('Cargando la lista de videojuegos...')
+  const videojuegos = await Videojuego.find(); //busca todos los videojuegos en la base de datos y los devuelve
+  return res.send(videojuegos)
 })
 app.get('/crear', async (_req, res) => {
-  console.log('creando...')
-  await Animal.create({ tipo: 'gatos', estado: 'Feliz' }) //crea un animal el cual va a ser tipo ANIMAL y su estado va a ser FELIZ, y luego nos devuelve un OK
+  console.log('Agregando nuevo videojuego...')
+  await Videojuego.create({ titulo: 'The Legend of Zelda', genero: 'Aventura' }) //crea un videojuego con título y género, y luego nos devuelve un OK
   return res.send('ok')
 })
 
-app.listen(3000, () => console.log('listening...')) //dejamos que se quede escuchando en el puerto 3000 y dejamos el consolelog para verificar que está corriendo la aplicacion
+app.listen(3000, () => console.log('Escuchando en el puerto 3000...')) //dejamos que se quede escuchando en el puerto 3000 y dejamos el consolelog para verificar que está corriendo la aplicacion
